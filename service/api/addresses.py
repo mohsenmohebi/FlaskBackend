9fc218b314cd7861165570d0ea132609042206f2
Mohsen <mohsenmohebi@gmail.com>
2021-12-08 11:00:50 -0800

    first commit;

--- service/api/addresses.py
+++ service/api/addresses.py
@@ -1,6 +1,7 @@
 import logging
 
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
+from os import error
 
 from flask import abort, jsonify
 from webargs.flaskparser import use_args
@@ -10,6 +11,7 @@ from marshmallow import Schema, fields
 from service.server import app, db
 from service.models import AddressSegment
 from service.models import Person
+from dateutil import parser
 
 
 class GetAddressQueryArgsSchema(Schema):
@@ -39,8 +41,13 @@ def get_address(args, person_id):
     elif len(person.address_segments) == 0:
         abort(404, description="person does not have an address, please create one")
 
-    address_segment = person.address_segments[-1]
-    return jsonify(AddressSchema().dump(address_segment))
+    # Commented by Mohsen
+    # address_segment = person.address_segments[-1]
+    # return jsonify(AddressSchema().dump(address_segment))
+
+    # added by Mohsen
+    address_segments = person.address_segments
+    return jsonify(AddressSchema(many=True).dump(address_segments))
 
 
 @app.route("/api/persons/<uuid:person_id>/address", methods=["PUT"])
@@ -66,11 +73,76 @@ def create_address(payload, person_id):
         db.session.commit()
         db.session.refresh(address_segment)
     else:
-        # TODO: Implementation
-        # If there are one or more existing AddressSegments, create a new AddressSegment
-        # that begins on the start_date provided in the API request and continues
-        # into the future. If the start_date provided is not greater than most recent
-        # address segment start_date, raise an Exception.
-        raise NotImplementedError()
+        start_date = payload.get("start_date")
+        address_segments = person.address_segments
+        addresses = AddressSchema(many=True).dump(address_segments)
+        skipInsert = False
+
+        for i in range(len(addresses)):
+            address = addresses[i]
+
+            # check if the start date exists
+            if start_date == parser.parse(str(address["start_date"])).date():
+                app.logger.error(
+                    "*********************************Please check here***************** "
+                )
+                app.logger.error(
+                    start_date == parser.parse(str(address["start_date"])).date()
+                )
+                app.logger.error(
+                    "*********************************Please check above***************** "
+                )
+                abort(422, description="address date does exist")
+            # check if the address is redundant ..
+            if (
+                payload.get("street_one") == address["street_one"]
+                and payload.get("street_two") == address["street_two"]
+                and payload.get("city") == address["city"]
+                and payload.get("state") == address["state"]
+                and payload.get("zip_code") == address["zip_code"]
+            ):
+
+                skipInsert = True
+                app.logger.error(f"skipInesrt is: {skipInsert}")
+                break
+
+        if not skipInsert:
+            address_segment = AddressSegment(
+                street_one=payload.get("street_one"),
+                street_two=payload.get("street_two"),
+                city=payload.get("city"),
+                state=payload.get("state"),
+                zip_code=payload.get("zip_code"),
+                start_date=payload.get("start_date"),
+                person_id=person_id,
+            )
+            db.session.add(address_segment)
+            db.session.commit()
+            db.session.refresh(address_segment)
 
     return jsonify(AddressSchema().dump(address_segment))
+    # return jsonify(addresses)
+
+
+@app.route("/api/persons/<uuid:person_id>/address/search", methods=["POST"])
+@use_args(AddressSchema())
+def get_address_bydate(payload, person_id):
+    app.logger.error(
+        "*********************************Please check here***************** "
+    )
+    person = Person.query.get(person_id)
+    if person is None:
+        abort(404, description="person does not exist")
+
+    address_segments = person.address_segments
+    addresses = AddressSchema(many=True).dump(address_segments)
+    # search_date = payload.get("search_date")
+    # app.logger.error("*********************************Please check here***************** ")
+    # app.logger.error(str(search_date))
+    # app.logger.error("*********************************Please check above***************** ")
+    # found_addresses = []
+    # for address in addresses:
+    #     if search_date == parser.parse(str(address["start_date"])).date():
+    #         found_addresses.append(address)
+    # found_addresses = filter(lambda x:str(x["start_date"])).date() == search_date,addresses)
+    return jsonify(addresses)
